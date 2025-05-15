20a7e1aa415d439bd50dc7038c6b74e71607ecee
Chirag Tailor <ctailor2@gmail.com>
2025-05-15 10:47:50 -0500

    Initial commit with generated spring boot project

---
+++ src/main/java/com/mongodbmodfactory/kitchensink_boot/KitchensinkBootApplication.java
@@ -0,0 +1,13 @@
+package com.mongodbmodfactory.kitchensink_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchensinkBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitchensinkBootApplication.class, args);
+	}
+
+}
